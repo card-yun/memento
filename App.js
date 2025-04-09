96c30deb7a05e133e07c7b4bdce01408faeba8fb
yge <goeun.yun97@gmail.com>
2025-04-10 02:17:10 +0900

    ScrollView (fixed)

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, ScrollView, Text, StyleSheet, Dimensions } from "react-native";
+// Components(View etc..) & APIs(StyleSheet, Dimensions)
+// Dimensions: size of phone screen
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+//console.log(windowWidth);
 
 export default function App() {
   return (
@@ -8,12 +14,44 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView // has so many Props!! See the Docs!!
+        horizontal
+        pagingEnabled // sticky & created scroll indicator_below (none: (too?) naturally)
+        showsHorizontalScrollIndicator={false} // hided indicator_below
+        contentContainerStyle={styles.weather}
+      >
+        {/*reactnative.dev/docs -> ScrollView (kind of View) -> Sellected 'horizontal' of Many Props */}
+        {/* Don't Memorize these all Prop things! Just Practice and Practice More!
+        by Reading Documents!! */}
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -33,12 +71,12 @@ const styles = StyleSheet.create({
     fontWeight: 500,
   },
   weather: {
-    flex: 2.5,
+    //ScrollView는 Screen을 벗어나야 하므로 flex없어야 함. 안 하면 에러남.
+    backgroundColor: "lightblue",
   },
   day: {
-    flex: 1,
+    width: windowWidth,
     alignItems: "center",
-    backgroundColor: "teal",
   },
   temp: {
     marginTop: 50,
